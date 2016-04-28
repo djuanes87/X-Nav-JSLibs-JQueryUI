0c3fabe07e931fc2048c6b47cc83c800e2d813d1
Daniel Juanes Quintana <d.juanes@alumnos.urjc.es>
2016-04-28 14:28:18 +0200

    Dragable

--- interfaz.js
+++ interfaz.js
@@ -1,25 +1,41 @@
 $(document).ready(function(){
   var menu = true;
+  var calendar = true;
 
   var fecha = function(){
     var hoy = new Date();
-    $("#date p" ).text(hoy.getDate() + "/" + hoy.getMonth() + "/" + hoy.getFullYear());
+    var text = $("#date button").text();
+    console.log(text);
+    $("#date button" ).append(hoy.getDate() + "/" + hoy.getMonth() + "/" + hoy.getFullYear());
 
   };
 
-  var desplegarMenu = function(){
-    if(menu){
-      $("#cuadro").hide(500);
-      menu = false;
+  var mostrarOcultar = function(id, bol){
+    if(bol){
+      $(id).hide(500);
     }else{
-      $("#cuadro").show(500);
-      menu = true;
+      $(id).show(500);
     }
   };
 
 
   $( "#menu" ).menu();
-  $("#home").click(desplegarMenu);
+  $("#home").click(function(){
+    mostrarOcultar("#cuadro", menu);
+    if(menu){
+      menu = false;
+    }else{
+      menu = true;
+    }
+  });
+  $("#date button").click(function(){
+    mostrarOcultar("#cuadro2", calendar);
+    if(calendar){
+      calendar = false;
+    }else{
+      calendar = true;
+    }
+  });
   $( "#datepicker" ).datepicker({
     showOn: "button",
     buttonImage: "jquery-ui-1.11.4.custom/images/calendar.gif",
@@ -28,7 +44,118 @@ $(document).ready(function(){
   });
 
 
-  desplegarMenu();
+
+
+  var $gallery = $( "#gallery" ),
+  $trash = $( "#trash" );
+
+// let the gallery items be draggable
+$( "li", $gallery ).draggable({
+  cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+  revert: "invalid", // when not dropped, the item will revert back to its initial position
+  containment: "document",
+  helper: "clone",
+  cursor: "move"
+});
+
+// let the trash be droppable, accepting the gallery items
+$trash.droppable({
+  accept: "#gallery > li",
+  activeClass: "ui-state-highlight",
+  drop: function( event, ui ) {
+    deleteImage( ui.draggable );
+  }
+});
+
+// let the gallery be droppable as well, accepting items from the trash
+$gallery.droppable({
+  accept: "#trash li",
+  activeClass: "custom-state-active",
+  drop: function( event, ui ) {
+    recycleImage( ui.draggable );
+  }
+});
+
+// image deletion function
+var recycle_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Recycle this image' class='ui-icon ui-icon-refresh'>Recycle image</a>";
+function deleteImage( $item ) {
+  $item.fadeOut(function() {
+    var $list = $( "ul", $trash ).length ?
+      $( "ul", $trash ) :
+      $( "<ul class='gallery ui-helper-reset'/>" ).appendTo( $trash );
+
+    $item.find( "a.ui-icon-trash" ).remove();
+    $item.append( recycle_icon ).appendTo( $list ).fadeIn(function() {
+      $item
+        .animate({ width: "48px" })
+        .find( "img" )
+          .animate({ height: "36px" });
+    });
+  });
+}
+
+// image recycle function
+var trash_icon = "<a href='link/to/trash/script/when/we/have/js/off' title='Delete this image' class='ui-icon ui-icon-trash'>Delete image</a>";
+function recycleImage( $item ) {
+  $item.fadeOut(function() {
+    $item
+      .find( "a.ui-icon-refresh" )
+        .remove()
+      .end()
+      .css( "width", "96px")
+      .append( trash_icon )
+      .find( "img" )
+        .css( "height", "72px" )
+      .end()
+      .appendTo( $gallery )
+      .fadeIn();
+  });
+}
+
+// image preview function, demonstrating the ui.dialog used as a modal window
+function viewLargerImage( $link ) {
+  var src = $link.attr( "href" ),
+    title = $link.siblings( "img" ).attr( "alt" ),
+    $modal = $( "img[src$='" + src + "']" );
+
+  if ( $modal.length ) {
+    $modal.dialog( "open" );
+  } else {
+    var img = $( "<img alt='" + title + "' width='384' height='288' style='display: none; padding: 8px;' />" )
+      .attr( "src", src ).appendTo( "body" );
+    setTimeout(function() {
+      img.dialog({
+        title: title,
+        width: 400,
+        modal: true
+      });
+    }, 1 );
+  }
+}
+
+// resolve the icons behavior with event delegation
+$( "ul.gallery > li" ).click(function( event ) {
+  var $item = $( this ),
+    $target = $( event.target );
+
+  if ( $target.is( "a.ui-icon-trash" ) ) {
+    deleteImage( $item );
+  } else if ( $target.is( "a.ui-icon-zoomin" ) ) {
+    viewLargerImage( $target );
+  } else if ( $target.is( "a.ui-icon-refresh" ) ) {
+    recycleImage( $item );
+  }
+
+  return false;
+});
+
+
+  $( "#ul.gallery li" ).draggable();
+
+  mostrarOcultar("#cuadro", menu);
+  menu = false;
+  mostrarOcultar("#cuadro2", calendar);
+  calendar = false;
   fecha();
 
 });
